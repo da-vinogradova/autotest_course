8a34ea2bdb721d0c70a37649fd82b29ab786c6d0
da-vinogradova <dashylia.vinogradova@yandex.ru>
2023-05-25 01:38:57 +0300

    №1 в дз7 выполнен

--- Homework_7/task_1.py
+++ Homework_7/task_1.py
@@ -8,8 +8,55 @@
 # Segment((2, 3), (4, 5)).length() --> 2.83
 # Segment((-2, -3), (4, 5)).x_axis_intersection() --> True
 # Segment((-2, -3), (4, -5)).y_axis_intersection() --> False
+import math
 
-# Здесь пишем код
+
+class Segment:
+
+    def __init__(self, segm1, segm2):
+        """
+        Конструктор для класса Segment, который определяет отрезок
+        :param segm1: координаты первой точки отрезка
+        :param segm2: координаты второй точки отрезка
+        """
+        self.segm1 = segm1
+        self.segm2 = segm2
+
+    def length(self):
+        """
+        Метод возвращает длину нашего отрезка, с округлением до 2 знаков после запятой
+        """
+        x1 = self.segm1[0]
+        y1 = self.segm1[1]
+        x2 = self.segm2[0]
+        y2 = self.segm2[1]
+        a = round(math.sqrt((x2-x1)**2+(y2-y1)**2), 2)
+        return a
+
+    def x_axis_intersection(self):
+        """
+        возвращает True, если отрезок пересекает ось абцисс, иначе False
+        """
+        y1 = self.segm1[1]
+        y2 = self.segm2[1]
+        # если координаты по разным сторонам от 0, то значит пересекают ось
+        if (y1 >= 0 > y2) or (y2 >= 0 > y1):
+            return True
+        else:
+            return False
+
+    def y_axis_intersection(self):
+        """
+        возвращает True, если отрезок пересекает ось ординат, иначе False
+        """
+        x1 = self.segm1[0]
+        x2 = self.segm2[0]
+        # если координаты по разным сторонам от 0, то значит пересекают ось
+        if (x1 >= 0 > x2) or (x2 >= 0 > x1):
+            return True
+        else:
+            return False
+
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 
