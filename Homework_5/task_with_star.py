99ccc1edaa1e0254ee70f325059fc9da2fa95878
da-vinogradova <dashylia.vinogradova@yandex.ru>
2023-05-28 01:50:34 +0300

    звездочка в дз5 выполнена

--- Homework_5/task_with_star.py
+++ Homework_5/task_with_star.py
@@ -11,11 +11,37 @@
 
 
 def to_roman(val):
-    # Здесь нужно написать код
+    """
+    Функция перевода арабских цифр в риские
+    :param val: int арабское число
+    :return: str римское число
+    """
+    # Список для перевода
+    list_roma_num = [('I', 'V'), ('X', 'L'), ('C', 'D'), ('M',)]
+    roman_str = ''
+    # Максимальное количество разрядов
+    num_len = len(str(val)) - 1
+
+    while val > 0:
+        # выделяем первую цифру, которую сейчас будем переводить
+        number = val // 10**num_len
+        # остаток, который продолжим делить
+        val = val % 10**num_len
+        if number < 4:
+            # напишем первое число из словаря number раз и добавим то, что уже написано
+            roman_str += list_roma_num[num_len][0] * number
+        elif number == 4:
+            roman_str += list_roma_num[num_len][0] + list_roma_num[num_len][1]
+        elif 5 <= number < 9:
+            roman_str += list_roma_num[num_len][1] + list_roma_num[num_len][0] * (number - 5)
+        else:
+            roman_str += list_roma_num[num_len][0] + list_roma_num[num_len + 1][0]
+        num_len -= 1
+
+    print(roman_str)
     return roman_str
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
-
 
 data = [1133, 2224, 1938, 1817, 2505, 391, 3743, 1634, 699, 1666, 1494, 1444]
 
