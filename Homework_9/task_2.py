778f4f7e99e999459bbcbfd88373f43d5ac5dc3e
da-vinogradova <dashylia.vinogradova@yandex.ru>
2023-06-04 01:03:24 +0300

    №2 из дз9 выполнено (со звездочкой)

--- Homework_9/task_2.py
+++ Homework_9/task_2.py
@@ -32,6 +32,7 @@
 
 
 import datetime
+import time
 from pathlib import Path
 
 
@@ -39,7 +40,14 @@ def func_log(func_to_decor):
     """Декоратор, который может принимать аргумент file_log (Путь до файла)"""
 
     def wrapper(file_log='log.txt'):
-        res = func_to_decor.__name__ + " вызвана " + func_to_decor(file_log)
+        """Декоратор, который может принимать аргумент file_log (Путь до файла)"""
+        # Здесь перебиваем название декоратора и пишем именно функцию, которую вызываем. и также хелп
+        wrapper.__name__ = func_to_decor.__name__
+        wrapper.__doc__ = func_to_decor.__doc__
+
+        # Строка, которая берет имя декорируемой функции и добавляет время ее вызова
+        res = func_to_decor.__name__ + " вызвана " + datetime.datetime.now().strftime("%d.%m %H:%M:%S")
+        func_to_decor(file_log)
         # Теперь нашу доработанную функцию надо записать в файл
         # Создаем относительный путь до файла с заданием. Из-за того, что не знаем слэши, разбиваем папки через запятую
         path = Path.cwd().joinpath(file_log)
@@ -50,9 +58,15 @@ def func_log(func_to_decor):
 
 def my_file(file_log):
     """ Функция возвращает время, когда была вызвана в формате %d.%m %H:%M:%S"""
-    call_time = datetime.datetime.now().strftime("%d.%m %H:%M:%S")
-    return call_time
+    time.sleep(3)
+    # call_time = datetime.datetime.now().strftime("%d.%m %H:%M:%S")
+    # return call_time
 
 
-my_file = func_log(my_file)
-my_file()
+f = func_log(my_file)
+f()
+
+# help, когда нет декоратора
+help(my_file)
+# help, когда есть декоратор
+help(f)
