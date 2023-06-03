3a6fd80cf825f406904e3059ec34318e828c5375
da-vinogradova <dashylia.vinogradova@yandex.ru>
2023-06-03 12:09:12 +0300

    Поправила задание 1

--- Homework_9/task_1.py
+++ Homework_9/task_1.py
@@ -5,29 +5,35 @@ from pathlib import Path
 
 # Создаем относительный путь до файла с заданием. Из-за того, что не знаем слэши, разбиваем папки через запятую
 path = Path('test_file', 'task1_data.txt')
+# Относительный путь до файла с ответом
 path_new = Path('test_file', 'task1_answer.txt')
 
 # Создаем абсолютный путь, чтобы работало все и везде. Сджойним путь до текущей рабочей
 # директории с относительным путем файла
 abs_path = Path.cwd().joinpath(path)
 
-# откроем файл и сразу записываем его содержимое в строку
+# откроем файл с данными
 task1_file = open(abs_path)
-task1_text = task1_file.read()
 
 # Создадим новый файл, чтобы в него сразу писать инфу
 new_file = open(Path.cwd().joinpath(path_new), "w+")
 
-# Список с цифрами
-list_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-# Удаляем цифры из строки
-for let in task1_text:
-    if let not in list_num:
-        new_file.write(let)
+try:
+    # Записываем содержимое исходного файла в строку
+    task1_text = task1_file.read()
+
+    # Список с цифрами
+    list_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    # Удаляем цифры из строки
+    for let in task1_text:
+        if let not in list_num:
+            new_file.write(let)
 
 # Закрываем все файлы
-new_file.close()
-task1_file.close()
+finally:
+    new_file.close()
+    task1_file.close()
 
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
