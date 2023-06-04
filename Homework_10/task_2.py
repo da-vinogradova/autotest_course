055cbfae7bda3055c71db5e8b72b84c2ae206b93
da-vinogradova <dashylia.vinogradova@yandex.ru>
2023-06-05 01:31:17 +0300

    №2 из дз10 выполнено

--- Homework_10/task_2.py
+++ Homework_10/task_2.py
@@ -15,3 +15,27 @@ def all_division(*arg1):
     for i in arg1[1:]:
         division /= i
     return division
+
+@pytest.mark.smoke
+def test_div_int():
+    assert all_division(16, 4, 2, 1) == 2
+
+@pytest.mark.without_zero
+def test_div_double():
+    assert all_division(10, 1.6) == 6.25
+
+@pytest.mark.smoke
+def test_negative():
+    assert all_division(-7, 1) == -7
+
+
+@pytest.mark.with_zero
+def test_zero_div():
+    assert all_division(0, 4) == 0
+
+@pytest.mark.with_zero
+def test_div_zero():
+    # Указали, что в результате выполнения теста должно упасть исключение
+    with pytest.raises(ZeroDivisionError):
+        all_division(5, 0)
+
