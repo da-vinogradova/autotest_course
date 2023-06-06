287c4547866c64ee3b13da1d9b96f2e78de035f0
da-vinogradova <dashylia.vinogradova@yandex.ru>
2023-06-07 00:14:59 +0300

    №4 из дз 10

--- conftest.py
+++ conftest.py
@@ -3,18 +3,14 @@ import datetime
 import pytest
 
 
+# Надо с этой что-то делать!!!!! Не работает
 @pytest.fixture(scope='class')
-def setup_class():
+def fixture_for_class():
     start_time = datetime.datetime.now()
     print('Время начала выполнения класса: ', start_time)
     yield start_time
-    print('Время окончания выполнения класса: ', datetime.datetime.now() - start_time)
-
-
-# @pytest.fixture()
-# def teardown_class():
-#     print('Время окончания выполнения класса: ', datetime.datetime.now())
-
+    end_time = datetime.datetime.now()
+    print('Время окончания выполнения класса: ', end_time)
 
 @pytest.fixture()
 def test_exec_time():
