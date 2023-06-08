1d9a1c57e4996e0e184c953597a781c753f60474
da-vinogradova <dashylia.vinogradova@yandex.ru>
2023-06-08 17:36:13 +0300

    №4 со звездочкой

--- Homework_10/task_with_star.py
+++ Homework_10/task_with_star.py
@@ -6,7 +6,6 @@ import pytest
 
 
 @pytest.mark.id_check(1, 2, 3)
-def test():
-    a = pytest.mark.__doc__
-    print(a)
-    print("В маркер передано: ", )
+def test(request):
+    a = request.node.own_markers[0].args
+    print("В маркер передано: ", a)
