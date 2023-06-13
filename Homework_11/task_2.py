a70b189afae95eabc0a4685ec1ae0c190546dcff
da-vinogradova <dashylia.vinogradova@yandex.ru>
2023-06-14 01:38:52 +0300

    Доработка 11 задания. Чуть чуть звездочки, чтобы не потерялась

--- Homework_11/task_2.py
+++ Homework_11/task_2.py
@@ -8,7 +8,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
-import time
+from selenium.webdriver import Keys
+from time import sleep
 
 url_fix = 'https://fix-online.sbis.ru/auth/'
 my_login = 'Oduvanchik2'
@@ -22,63 +23,54 @@ driver.maximize_window()
 try:
     # Авторизоваться на сайте https://fix-online.sbis.ru/
     driver.get(url_fix)
-    time.sleep(1)
+    sleep(1)
     login = driver.find_element(By.CSS_SELECTOR, '[name="Login"]')
-    login.send_keys(my_login)
+    login.send_keys(my_login, Keys.ENTER)
     assert login.get_attribute('value') == my_login
-    first_auth_btn = driver.find_element(By.CSS_SELECTOR, '[data-qa="auth-AdaptiveLoginForm__checkSignInTypeButton"]')
-    first_auth_btn.click()
-    time.sleep(1)
+
     pass_on_page = driver.find_element(By.CSS_SELECTOR, '[name="Password"]')
-    pass_on_page.send_keys(my_pass)
-    time.sleep(1)
-    assert pass_on_page.get_attribute('value') == my_pass
-    login_btn = driver.find_element(By.CSS_SELECTOR, '[data-qa="auth-AdaptiveLoginForm__signInButton"]')
-    login_btn.click()
-    time.sleep(7)
+    pass_on_page.send_keys(my_pass, Keys.ENTER)
+    sleep(7)
     # my_mac = driver.find_element(By.XPATH, "//*[contains(text(), 'Нет, личное')]")
     # my_mac.click()
     # time.sleep(2)
     # Перейти в реестр Контакты
     page_contacts = driver.find_element(By.CSS_SELECTOR, '[href="/page/dialogs"]')
-    time.sleep(2)
     page_contacts.click()
-    time.sleep(5)
+    sleep(2)
     driver.find_element(By.XPATH, "//*[contains(text(), 'Контакты')]").click()
     # Отправить сообщение самому себе
-    time.sleep(5)
+    sleep(1)
     add_btn = driver.find_element(By.CSS_SELECTOR, '[data-qa="sabyPage-addButton"]')
     add_btn.click()
-    time.sleep(2)
+    sleep(1)
     search = driver.find_element(By.CSS_SELECTOR, 'input[tabindex="0"]')
     search.send_keys(my_name)
-    time.sleep(1)
     driver.find_element(By.CSS_SELECTOR, '[data-qa = "item"]').click()
-    time.sleep(2)
+    sleep(4)
     # Написать сообщение
     text_msg = driver.find_element(By.CSS_SELECTOR, '[data-qa="textEditor_slate_Field"]')
     text_msg.send_keys(my_text)
-    time.sleep(1)
     driver.find_element(By.CSS_SELECTOR, '[data-qa="msg-send-editor__send-button"]').click()
-    time.sleep(2)
+    sleep(2)
     # Убедиться, что сообщение появилось в реестре
     new_msg_aut = driver.find_elements(By.CSS_SELECTOR, '[data-qa="msg-dialogs-item__addressee')
     assert new_msg_aut[0].text == my_name
 
     first_msg_txt = driver.find_elements(By.CSS_SELECTOR, '.msg-dialogs-item__content-inner')
     assert first_msg_txt[0].text == my_text
-    time.sleep(5)
+    sleep(4)
 
     # Удалить это сообщение и убедиться, что удалили
     action_chains = ActionChains(driver)
     action_chains.move_to_element(first_msg_txt[0])
     action_chains.context_click(first_msg_txt[0])
     action_chains.perform()
-    time.sleep(1)
+    sleep(1)
 
     delete_button = driver.find_element(By.CSS_SELECTOR, '[data-target = "menu_item_deleteToArchive"]')
     delete_button.click()
-    time.sleep(2)
+    sleep(2)
 
     first_msg_txt = driver.find_elements(By.CSS_SELECTOR, '.msg-dialogs-item__content-inner')
     assert first_msg_txt[0].text != my_text, "Сообщение не удалено"
--- Homework_11/task_with_star.py
+++ Homework_11/task_with_star.py
@@ -9,3 +9,34 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
+
+sbis_url = 'https://sbis.ru/'
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+try:
+    # Перейти на  https://sbis.ru/
+    driver.get(sbis_url)
+    sleep(1)
+    # В Footer'e найти "Скачать СБИС"
+    update_in_foot = driver.find_element(By.XPATH, '//a[text()="Скачать СБИС"]')
+    assert update_in_foot.accessible_name == 'Скачать СБИС'
+    assert update_in_foot.text == 'Скачать СБИС'
+    # Перейти по ней
+    driver.get(update_in_foot.get_attribute("href"))
+    # Скачать СБИС Плагин для вашей ОС в папку с данным тестом
+    driver.find_element(By.CSS_SELECTOR, '[data-id="plugin"]').click()
+    sleep(1)
+
+    # Отсюда что-то идет не так
+    mac_os = driver.find_element(By.CSS_SELECTOR, '[data-id="macos"]')
+
+    driver.find_element(By.XPATH, '//a[text() = "Скачать для Apple Silicon (Pkg 269.12 МБ) "]').click()
+    # Убедиться, что плагин скачался
+
+    sleep(2)
+    print("Все ок")
+finally:
+    driver.quit()
